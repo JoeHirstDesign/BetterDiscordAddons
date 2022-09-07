dec57553a2892ea03fed6297371210d5eb9d52c5
JoeHirstDesign <103528093+JoeHirstDesign@users.noreply.github.com>
2022-09-07 12:30:09 -0400

    Add files via upload

---
+++ Plugins/DiscordPivacy/DiscordPrivacy.plugin.js
@@ -0,0 +1,22 @@
+/**
+ * @name Discord Privacy
+ * @version 0.0.1
+ * @authorId 189496837319819264
+ * @authorLink https://joe-hirst.com
+ * @website https://joe-hirst.com
+ * @description Discord Privacy adds basic full window privacy toggles and functionality to Discord!
+ * @author Joe Hirst 
+ */
+
+module.exports = class DiscordPrivacy {
+  constructor(meta) {
+    // Do stuff in here before starting
+    
+  }
+  start() {
+    // Do stuff when enabled
+  }
+  stop() {
+    // Cleanup when disabled
+  }
+};
